51262f6b64d3cf7cfdadabc1271bd061c2788394
Simon.H <835374624@qq.com>
2017-06-24 19:19:26 +0800

    Update index.js
    add emojione support

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ var emojies_shortcuts = require('./lib/data/shortcuts');
 var emoji_html        = require('./lib/render');
 var emoji_replace     = require('./lib/replace');
 var normalize_opts    = require('./lib/normalize_opts');
-try {
-    console.log(require.resolve("twemoji"));
-} catch(e) {
-    console.error("run git clone https://github.com/twitter/twemoji.git under markdown-it-emoji first!");
-}
-var twemoji = require('twemoji')
 
+var twemoji = require('twemoji')
+var emojione = require('emojione');
+var cheerio = require('cheerio');
 
 module.exports = function emoji_plugin(md, options) {
   var defaults = {
@@ -25,10 +22,25 @@ module.exports = function emoji_plugin(md, options) {
 
   //md.renderer.rules.emoji = emoji_html;
   
+  //md.renderer.rules.emoji = function(token, idx) {
+  //  return twemoji.parse(token[idx].content, {
+  //    className: "nofancybox emoji"
+  //  });
+  //};
+  
   md.renderer.rules.emoji = function(token, idx) {
-    return twemoji.parse(token[idx].content, {
-      className: "nofancybox emoji"
+    var result = emojione.unicodeToImage(token[idx].content); var cheerio = require('cheerio');
+    var $ = cheerio.load(result, {
+        ignoreWhitespace: false,
+        xmlMode: false,          
+        lowerCaseTags: false,      
+        decodeEntities: false    
     });
+    $('img').removeAttr('title');
+    $('img').addClass('nofancybox');
+    $('img').addClass('emoji');
+    $('img').attr('draggable', false);
+    return $.html();
   };
 
   md.core.ruler.push('emoji', emoji_replace(md, opts.defs, opts.shortcuts, opts.scanRE, opts.replaceRE));
